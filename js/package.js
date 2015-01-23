62c3f80f605403d6d34fcb4a58009fa784692567
Webnist <webnist@webnist.org>
2015-01-23 14:17:29 +0900

    first commit

---
+++ js/package.js
@@ -0,0 +1,22 @@
+(function($) {
+	/* Setting */
+	var $remoteDomain = 'http://managed.nattodaisuki.com/';
+	var $localDomain  = 'http://nattodaisuki.com/';
+	var $dir          = location.href.replace( $localDomain, '' ).split('/');
+
+
+	$.ajax({
+		type: 'GET',
+		url: $remoteDomain + 'wp-json/menus/',
+		dataType: 'json'
+	}).done(function(json) {
+		console.log(json);
+		var len = json.length;
+		for (var i = 0; i < len; i++) {
+			link = json[i].link.replace('managed.', '');
+			$(".ajax").append('<li><a href="' + link + '">' + json[i].title + '</a></li>');
+		}
+	}).fail(function(json) {
+		console.log(json);
+	});
+})(jQuery);
